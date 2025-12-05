31b7663709b4034c64291eac1a4a72f13a7138f1
draftworks-repo <draftworks000@gmail.com>
2025-12-05 20:33:00 +0530

    email works now mongo removed

--- src/app/components/RegistrationForm.tsx
+++ src/app/components/RegistrationForm.tsx
@@ -78,7 +78,7 @@ export const RegistrationForm: React.FC = () => {
       setIsSubmitting(true);
 
       try {
-        const response = await fetch("/api/send-mail", {
+        const response = await fetch("/api/send-email", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
